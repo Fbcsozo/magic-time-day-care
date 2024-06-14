ad143fad9cc7e7440e99921add44375b3fb293a1
fbcsozo <fabriciosozo@live.com>
2024-06-14 18:23:50 -0300

    twenty-second commit

--- src/components/Comments/comments.jsx
+++ src/components/Comments/comments.jsx
@@ -1,27 +1,26 @@
-
-
 import React from "react";
 import "./comments.css";
 
 const exampleComments = [
   {
-    name: "Alice Johnson",
+    name: "Camylla freitas",
     comment:
-      "Amazing daycare! My kids love it here and always come home with a smile.",
+      "Nathany has always stayed with my children when I needed to work, and for them, it was always good news since it meant fun times. She is very responsible and caring with my kids. She respects our schedules and is a very experienced nanny. I highly recommend her without hesitation. I am a nurse and a very cautious mother; they only stay with her. 🫶🏼",
   },
   {
-    name: "Mark Smith",
-    comment: "Great environment and friendly staff. Highly recommend!",
+    name: "Thais Costa",
+    comment:
+      "I recommend Magic Time to all parents who want their child to have a reliable, safe, clean place full of games that help their child's development.",
   },
   {
-    name: "Sarah Brown",
+    name: "Anna Ahmed",
     comment:
-      "My daughter has learned so much and made great friends. Fantastic place!",
+      "Nathany is the the absolute best person we could have asked to care for our daughter- she started with us when our daughter was 4 months old and has cared for her over the last 14 months. She helped with sleep training, nap transitions, solid food introduction and countless other milestones. We trust her completely with our daughter- I highly recommend her.",
   },
   {
-    name: "David Lee",
+    name: "Lorena Coelho",
     comment:
-      "Safe and nurturing place for children. Couldn't be happier with the care provided.",
+      "I highly recommend her without hesitation! Besides having extensive experience with children, Nathy is a nurse with several professional courses. Moms in the Bay Area can leave their children with her without worry. 🥰🥰",
   },
 ];
 
@@ -51,54 +50,3 @@ function Comments({ language }) {
 }
 
 export default Comments;
-
-/* commentários atualizados pelo chat gpt
-
-import React from "react";
-import "./comments.css";
-
-const exampleComments = [
-  {
-    name: "Alice Johnson",
-    comment: "Amazing daycare! My kids love it here and always come home with a smile.",
-  },
-  {
-    name: "Mark Smith",
-    comment: "Great environment and friendly staff. Highly recommend!",
-  },
-  {
-    name: "Sarah Brown",
-    comment: "My daughter has learned so much and made great friends. Fantastic place!",
-  },
-  {
-    name: "David Lee",
-    comment: "Safe and nurturing place for children. Couldn't be happier with the care provided.",
-  },
-];
-
-function Comments({ language }) {
-  const texts = {
-    pt: {
-      title1: "COMENTÁRIOS",
-    },
-    en: {
-      title1: "COMMENTS",
-    },
-  };
-
-  return (
-    <div id="comments" className="comment">
-      <h1 className="title"> {texts[language].title1} </h1>
-      <div className="comments-container">
-        {exampleComments.map((comment, index) => (
-          <div key={index} className="comment-box">
-            <p className="comment-name">{comment.name}</p>
-            <p className="comment-text">"{comment.comment}"</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
-
-export default Comments; */
